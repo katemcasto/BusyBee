ce3b5d9438bf6f2ef120f21c6f18a0e0b3285c24
katemcasto <Katemcasto@outlook.com>
2024-01-14 16:26:59 -0500

    Update Chores.sql
    Added completedBy and completedDate to chore creation script.

--- sql/Chores.sql
+++ sql/Chores.sql
@@ -2,6 +2,8 @@ CREATE TABLE [dbo].[Chores](
    [ID] [int] IDENTITY(1,1) NOT NULL, 
    [Description] [nvarchar](50) NULL,
    [Complete] [bit] NOT NULL,
+   [CompletedBy] [nvarchar](20) NOT NULL,
+   [CompletedDate] [datetime] NOT NULL,
    [CreatedBy] [nvarchar](20) NOT NULL,
    [CreatedDate] [datetime] NOT NULL,
    [UpdatedBy] [nvarchar](20) NOT NULL,
